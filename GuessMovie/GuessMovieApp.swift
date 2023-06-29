01d5d42ec9f5e495eb6caef3d573a97cb552689d
DeanRocket <zeroqazggc0504@gmail.com>
2023-06-29 10:18:02 +0800

    Initial Commit

---
+++ GuessMovie/GuessMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessMovieApp.swift
+//  GuessMovie
+//
+//  Created by Yo on 2023/6/29.
+//
+
+import SwiftUI
+
+@main
+struct GuessMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
